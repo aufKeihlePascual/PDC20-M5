1862a3fc3c4b487e9ba54fd8b836cb611d6f26e2
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-10-16 17:05:29 +0800

    Final Editing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
+import './event.css'
 // import UserProfilePage from './Profile/UserProfilePage.jsx'
 import MainEvent from './Events/MainEvent.jsx';
 
