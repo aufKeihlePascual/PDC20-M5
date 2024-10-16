4243aa543491335a5ad1cdd6ea9d67e1cc96156e
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-10-16 15:57:13 +0800

    Added events for keyboard

---
+++ src/Events/KeyboardEvent.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function KeyboardEvent() {
+    const handleKeyDown = (event) => {
+        if (event.key == "Enter") {
+            alert("Enter key pressed");
+        }
+    };
+
+    const handleKeyPress = (event) => {
+        console.log(`Key Pressed: ${event.key}`);
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Type Something"
+                onKeyDown={handleKeyDown}
+                onKeyPress={handleKeyPress}
+            />
+        </div>
+    )
+}
+
+export default KeyboardEvent;
