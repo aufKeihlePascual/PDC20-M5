8a06b79bbf921bbeec21ad5ef8381a1c8265ae67
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-10-16 16:10:44 +0800

    Added mouse events

--- src/Events/MainEvent.jsx
+++ src/Events/MainEvent.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import EventonClick from "./EventonClick";
 import KeyboardEvent from "./KeyboardEvent";
+import MouseEvent from "./MouseEvent";
 
 function MainEvent() {
     return (
         <div>
             <EventonClick/>
             <KeyboardEvent/>
+            <MouseEvent/>
         </div>
     );
 }
